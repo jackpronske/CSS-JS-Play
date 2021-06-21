cf1649428ccdc07cee7126d3cd58d03243059ae5
Jack <jackpronske@yahoo.com>
2021-06-20 20:02:47 -0700

    --last minute css

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const inputs = document.querySelectorAll('.controls input');
 
-
 function handleUpdate() {
   const suffix = this.dataset.sizing || '';
   document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
